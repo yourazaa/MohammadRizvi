aa4bd783c50990a90815968e6b231c16ee66e123
yourazaa <rizvimohammad77@gmail.com>
2021-06-02 12:46:21 +0530

    dd

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Web-Developer", "Designer", "Freelancer", "Content-writer"],
+        strings: [ "Web-Developer", "Designer", "Freelancer", "Content-writer","Digital Marketing"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Web-Developer", "Designer", "Freelancer", "Content-writer"],
+        strings: [ "Web-Developer", "Designer", "Freelancer", "Content-writer", "Digital Marketing"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
